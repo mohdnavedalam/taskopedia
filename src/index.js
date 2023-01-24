faa96baef51aadd04d06153eac25be69d95ab43e
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2023-01-24 19:14:40 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
+// import React from 'react';  ----- Not needed in React 18
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../src/CSS/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className='text-primary'>
+    Hello World.
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
