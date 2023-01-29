c734d64094a0b6bd176fd86cadb1ccde8788d176
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2023-01-29 22:10:31 +0530

    new components

---
+++ src/Components/Students.jsx
@@ -0,0 +1,24 @@
+export default function Students() {
+    const fullName = "Mohd Naved Alam";
+    const programmingExperience = 4;
+    return (
+      <div className="container p-4">
+        <div className='row'>
+          Students Enrolled
+        </div>
+        <div className='row border'>
+          <div className='col-2'>
+            <img
+              src={`https://ui-avatars.com/api/?name=${fullName}`}
+              className='w-100'>
+            </img>
+          </div>
+          <div className='col-10'>
+            {fullName}
+            <br />
+            Programming Experience - {programmingExperience} years.
+          </div>
+        </div>
+      </div>
+    );
+  }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 // import React from 'react';  ----- Not needed in React 18
 import ReactDOM from 'react-dom/client';
 import '../src/CSS/style.css';
+import Header from './Components/Header';
+import Students from './Components/Students';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function MainHeader() {
-  return <h1 className="heading1">REACT COURSE</h1>;
-}
-
-const subHeaderStyle = {
-  color: "blueviolet",
-  backgroundColor: "lightgray",
-};
-function SubHeader() {
-  return <p style={subHeaderStyle}>This will be an exciting course.</p>;
-}
-function Header() {
-  return (
-    <div>
-      <MainHeader></MainHeader>
-      <SubHeader></SubHeader>
-    </div>
-  );
-}
 
 function MainBody() {
   const whatWeWillLearn = "React JS";
@@ -39,31 +21,6 @@ function MainBody() {
         <li>Basic Foundation</li>
         <li>Functional and Class Components</li>
       </ul>
-    </div>
-  );
-}
-
-function Students() {
-  const fullName = "Mohd Naved Alam";
-  const programmingExperience = 4;
-  return (
-    <div className="container p-4">
-      <div className='row'>
-        Students Enrolled
-      </div>
-      <div className='row border'>
-        <div className='col-2'>
-          <img
-            src={`https://ui-avatars.com/api/?name=${fullName}`}
-            className='w-100'>
-          </img>
-        </div>
-        <div className='col-10'>
-          {fullName}
-          <br />
-          Programming Experience - {programmingExperience} years.
-        </div>
-      </div>
     </div>
   );
 }
