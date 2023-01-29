fcc4fe02c852fdbf6013c717cf99150376fde497
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2023-01-29 22:00:34 +0530

    lec-36

--- src/index.js
+++ src/index.js
@@ -39,8 +39,30 @@ function MainBody() {
         <li>Basic Foundation</li>
         <li>Functional and Class Components</li>
       </ul>
-      <div>
-        Enter Task : <input maxLength={20} readOnly={false} placeholder="Naved"></input>
+    </div>
+  );
+}
+
+function Students() {
+  const fullName = "Mohd Naved Alam";
+  const programmingExperience = 4;
+  return (
+    <div className="container p-4">
+      <div className='row'>
+        Students Enrolled
+      </div>
+      <div className='row border'>
+        <div className='col-2'>
+          <img
+            src={`https://ui-avatars.com/api/?name=${fullName}`}
+            className='w-100'>
+          </img>
+        </div>
+        <div className='col-10'>
+          {fullName}
+          <br />
+          Programming Experience - {programmingExperience} years.
+        </div>
       </div>
     </div>
   );
@@ -55,6 +77,7 @@ root.render(
   <div>
     <Header />
     <MainBody />
+    <Students />
     <Footer />
   </div>
 );
